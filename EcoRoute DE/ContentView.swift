13d3712da93fe0218ac8b76387f4c0a3d975c6a0
Aby Mathew <abymathew2142@gmail.com>
2026-06-28 22:55:36 +0200

    Initial Commit

---
+++ EcoRoute DE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EcoRoute DE
+//
+//  Created by Aby Mathew on 28/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EcoRoute DE/EcoRoute_DEApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoRoute_DEApp.swift
+//  EcoRoute DE
+//
+//  Created by Aby Mathew on 28/06/26.
+//
+
+import SwiftUI
+
+@main
+struct EcoRoute_DEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
