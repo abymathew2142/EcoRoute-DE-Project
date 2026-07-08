cf54042aa20af7795389d27ca927ac9acf1efa95
Aby Mathew <abymathew2142@gmail.com>
2026-07-08 22:58:53 +0200

    feature: creates Constants.swift file for maintain constants values with meaningful name without any magical number issues

---
+++ EcoRoute DE/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  EcoRoute DE
+//
+
+
+import Foundation
+
+struct Constants {
+    // Currency
+    static let currencyCode = "EUR"
+    
+    // Limits
+    static let maxCommuteDistance: Double = 250.0
+    static let minBikingDistance: Double = 50.0
+
+}
